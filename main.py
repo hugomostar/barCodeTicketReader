b1dfc1eade8bfdb59831f8042fb8bbfcc519ef44
Drazen Alpeza <drazen.a@gmail.com>
2016-02-19 16:15:44 +0100

    Update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from PyQt4 import QtCore, QtGui
 from viewController import *
 
-ticketNumber = '1111111111111'
+ticketNumber = '3111111111111'
 app = QtGui.QApplication(sys.argv)
 myapp = MyForm(ticketNumber)
 myapp.show()
