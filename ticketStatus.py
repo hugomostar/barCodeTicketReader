18d6c11a54bac3ec3d7d1ae07b081aefad54343d
Drazen Alpeza <drazen.a@gmail.com>
2016-02-25 16:56:38 +0100

    update

--- ticketStatus.py
+++ ticketStatus.py
@@ -3,7 +3,7 @@ import _mssql
 class ticketStatus():
 
     def checkTicketNumber(ticketNumber):
-        conn = _mssql.connect(server='192.168.0.25\SQLEXPRESS', user='sa', password='lozinka', \
+        conn = _mssql.connect(server='192.168.0.25:4910', user='sa', password='lozinka', \
                           database='eGate15Sql')
         sqlcmd = """
             DECLARE
