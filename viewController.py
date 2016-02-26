d012d176b8d25349cc9b0d7fb51a9092474f26df
Drazen Alpeza <drazen.a@gmail.com>
2016-02-26 12:43:12 +0100

    update

--- viewController.py
+++ viewController.py
@@ -3,6 +3,7 @@ import sys
 from PyQt4 import QtCore, QtGui
 from mainView import Ui_ticketReaderView, _translate
 from ticketStatus import *
+from PyQt4.QtCore import QTimer
 
 class MyForm(QtGui.QMainWindow):
 
@@ -12,6 +13,10 @@ class MyForm(QtGui.QMainWindow):
         QtGui.QWidget.__init__(self, parent)
         self.ui = Ui_ticketReaderView()
         self.ui.setupUi(self)
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.setDefault)
+
         #self.showFullScreen()
     def keyPressEvent(self, event):
          key = event.key()
@@ -21,11 +26,20 @@ class MyForm(QtGui.QMainWindow):
          if key == QtCore.Qt.Key_Return:
             print('Ticket:', self.ticketNumber)
             self.checkIsTicketValid()
+            self.timer.start(3000)
             self.ticketNumber = ''
+
+    def setDefault(self):
+        self.ui.textEdit.setHtml(_translate("ticketReaderView", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+                                            "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+                                            "p, li { white-space: pre-wrap; }\n"
+                                            "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+                                            "<p align=\"center\" style=\" margin-top:0px; margin-bottom:15px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; background-color:#eeeeee;\"><span style=\" font-family:\'Consolas,Menlo,Monaco,Lucida Console,Liberation Mono,DejaVu Sans Mono,Bitstream Vera Sans Mono,Courier New,monospace,sans-serif\'; font-size:28pt; font-weight:600; color:#3117c7; background-color:#eeeeee;\">Molimo nanesite <br /> vaš tiket<br /> </span></p></body></html>", None))
+
 
     def checkIsTicketValid(self):
         self.status = ticketStatus.checkTicketNumber(self.ticketNumber)
-        if self.status == 'ok':
+        if self.status == 'OK!':
             self.ui.textEdit.setHtml(_translate("ticketReaderView", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
                                                 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
                                                 "p, li { white-space: pre-wrap; }\n"
